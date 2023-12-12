c4194b598f3c29c396f71d95b5c8a121d4e25a16
rahul-9340 <142489789+rahul-9340@users.noreply.github.com>
2023-12-12 11:43:35 +0000

    new changes 2

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ let [count,setcount] = useState(0)
 
   return (
     <div>
-       <p>Button clicked {count} times </p>
+       <p>Button clicked {count} times</p>
        <button onClick={()=>{setcount(count+1)}}>Click Me</button>
     </div>
   )
